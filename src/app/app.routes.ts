28fad6b3f625e365672c8cea2bbd13603d0dd077
gian <gianmarco.clu23@gmail.com>
2024-05-20 14:57:35 +0200

    feat(login): login page implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './page/home-page/home-page.component';
+import { LoginComponent } from './page/login/login.component';
 
 export const routes: Routes = [
     { path:'home', component:HomePageComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: "login", component:LoginComponent, pathMatch: 'full' }
 ];
