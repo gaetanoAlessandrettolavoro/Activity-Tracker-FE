33a83e26d3dacfafa96fc0cc9764f154c4c3c47b
Rita <rita.ciaramitaro@stevejobs.academy>
2024-05-20 12:40:31 +0200

    feat(home):Home page implementation
    - Modified app.component.html, app.component.ts, and app.routes.ts to integrate the new home page
    - Created the home-page component with its corresponding CSS, HTML, spec.ts, and component.ts files
    - Added a new background for the home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from "./page/home-page/home-page.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HomePageComponent]
 })
 export class AppComponent {
   title = 'ActivityTracker-FE';
