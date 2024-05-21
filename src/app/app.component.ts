57ae32dd9a3cb42030471032b852959fb9b288ae
Gaetano Alessandretto <gaetanoalessandrettowork@gmail.com>
2024-05-21 15:54:30 +0200

    feat(footer):Fixed footer in component view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from "./page/home-page/home-page.component";
-import { FooterComponent } from './componenti/footer/footer.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HomePageComponent,FooterComponent]
+    imports: [RouterOutlet, HomePageComponent]
 })
 export class AppComponent {
   title = 'ActivityTracker-FE';
