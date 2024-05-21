9cd50e1cdc346c7204edf2bfc472686bee335d29
gian <gianmarco.clu23@gmail.com>
2024-05-21 12:51:27 +0200

    feat(noDailyActivity): no-daily-activity component implementation

---
+++ src/app/componenti/no-daily-activity/no-daily-activity.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'no-daily-activity',
+  standalone: true,
+  imports: [],
+  templateUrl: './no-daily-activity.component.html',
+  styleUrl: './no-daily-activity.component.css'
+})
+export class NoDailyActivityComponent {
+
+}
