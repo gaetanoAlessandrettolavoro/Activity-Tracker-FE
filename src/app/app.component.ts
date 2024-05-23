4fd6208562034cda92a2fea346e0281282ad67d6
Rita <rita.ciaramitaro@stevejobs.academy>
2024-05-23 17:55:03 +0200

    feat(attivitòRecentiUtente): -Implementation of recent activities page for user -Implementation of search filter for activity and date range -Implementation of table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from "./page/home-page/home-page.component";
+import { AttivitaRecentiUtenteComponent } from "./page/attivita-recenti-utente/attivita-recenti-utente/attivita-recenti-utente.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HomePageComponent]
+    imports: [RouterOutlet, HomePageComponent, AttivitaRecentiUtenteComponent]
 })
 export class AppComponent {
   title = 'ActivityTracker-FE';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './page/home-page/home-page.component';
+import { AttivitaRecentiUtenteComponent } from './page/attivita-recenti-utente/attivita-recenti-utente/attivita-recenti-utente.component';
 
 export const routes: Routes = [
     { path:'home', component:HomePageComponent },
+    { path:'attivitaRecentiUtente',component:AttivitaRecentiUtenteComponent},
 ];
