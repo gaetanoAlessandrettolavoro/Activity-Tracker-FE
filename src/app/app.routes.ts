4c2ee8df9dcfc74adf6ef25a70238d4997d838ce
gian <gianmarco.clu23@gmail.com>
2024-05-23 12:35:59 +0200

    feat(user-home): added main page when user will be logged

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './page/home-page/home-page.component';
 import { LoginComponent } from './page/login/login.component';
 import { RegisterComponent } from './page/page/register/register.component';
+import { UserHomeComponent } from './page/user-home/user-home.component';
 
 export const routes: Routes = [
     { path:'home', component:HomePageComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: "login", component:LoginComponent, pathMatch: 'full' },
+    { path: "login/oggi", component:UserHomeComponent, pathMatch: 'full'},
     { path: "registrati", component: RegisterComponent, pathMatch: 'full'}
 ];
