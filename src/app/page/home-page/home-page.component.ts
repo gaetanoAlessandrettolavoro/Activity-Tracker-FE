646c895027bae1e4fb7971950bdc79b2a6aae974
Gaetano Alessandretto <gaetanoalessandrettowork@gmail.com>
2024-05-27 17:19:26 +0200

    first test

--- src/app/page/home-page/home-page.component.ts
+++ src/app/page/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarhomeComponent } from '../../componenti/navbarhome/navbarhome.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [NavbarhomeComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
