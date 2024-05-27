f8d380468bc4b916d634173af82f05dedb01dec3
Gaetano Alessandretto <gaetanoalessandrettowork@gmail.com>
2024-05-27 16:05:25 +0200

    fix(fix-regex-codfisc):fixed the tax code pattern

--- src/app/page/page/register/register.component.ts
+++ src/app/page/page/register/register.component.ts
@@ -88,9 +88,9 @@ export class RegisterComponent {
       TaxIDcode: new FormControl('', [
         Validators.required,
         Validators.pattern(
-          /^[A-Za-z]{6}[0-9]{2}[A-Za-z]{1}[0-9]{2}[A-Za-z]{1}[0-9]{3}[A-Za-z]{1}$/
+          '^(?:[A-Z][AEIOU][AEIOUX]|[AEIOU]X{2}|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$'
         ),
-      ]),
+      ]),      
       email: new FormControl('', [
         Validators.required,
         Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}$'),
