0598603a753d39eb97a36bfefe9f0dc7628ddb48
Gaetano Alessandretto <gaetanoalessandrettowork@gmail.com>
2024-05-28 11:07:18 +0200

    feat(add-modal-info):inserted modal info in all navbars

--- src/app/componenti/navbarattgiornaliere/navbarattgiornaliere.component.ts
+++ src/app/componenti/navbarattgiornaliere/navbarattgiornaliere.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
 import { ButtonModule } from 'primeng/button'
+import { UserManualComponent } from '../user-manual/user-manual.component';
 
 @Component({
   selector: 'navbarattgiornaliere',
   templateUrl: './navbarattgiornaliere.component.html',
   styleUrls: ['./navbarattgiornaliere.component.css'],
   standalone: true,
-  imports: [MenubarModule, ButtonModule]
+  imports: [MenubarModule, ButtonModule,UserManualComponent]
 })
 export class NavbarComponent{
 
