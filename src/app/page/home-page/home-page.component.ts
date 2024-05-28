60694d5d70df9eb8f42c4082b409a95ef3c3d500
Rita <rita.ciaramitaro@stevejobs.academy>
2024-05-28 17:11:13 +0200

    feat(navbar/footer): Added navbar and footer in the ‘attività recenti’ page and in the ‘home-page' fix(footer/navbar): fixed footers and navbars

--- src/app/page/home-page/home-page.component.ts
+++ src/app/page/home-page/home-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarhomeComponent } from '../../componenti/navbarhome/navbarhome.component';
+import { FooterComponent } from "../../componenti/footer/footer.component";
 
 @Component({
-  selector: 'app-home-page',
-  standalone: true,
-  imports: [NavbarhomeComponent],
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+    selector: 'app-home-page',
+    standalone: true,
+    templateUrl: './home-page.component.html',
+    styleUrl: './home-page.component.css',
+    imports: [NavbarhomeComponent, FooterComponent]
 })
 export class HomePageComponent {
 
