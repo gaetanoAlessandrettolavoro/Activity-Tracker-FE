d75f11aa43347fe3ec11bfa8033fd7137d76fe03
gian <gianmarco.clu23@gmail.com>
2024-05-28 12:37:11 +0200

    fix(edit-activity): updated typification using new activityModel

--- src/app/componenti/edit-activity-button/edit-activity-button.component.ts
+++ src/app/componenti/edit-activity-button/edit-activity-button.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 import { EditActivityFormComponent } from '../edit-activity-form/edit-activity-form.component';
+import { Activity } from '../../model/activityModel';
 
 @Component({
   selector: 'edit-activity',
@@ -13,7 +14,7 @@ import { EditActivityFormComponent } from '../edit-activity-form/edit-activity-f
 export class EditActivityButtonComponent implements OnInit{
   visible: boolean = false;
 
-  @Input({required: true}) activity!: any; //DA TIPIZZARE
+  @Input({required: true}) activity!: Activity;
 
   showDialog() {
     this.visible = true;
