ed4f0319cab65e1062e43db77e550e191ac87d8b
Rita <rita.ciaramitaro@stevejobs.academy>
2024-06-05 09:51:24 +0200

    feat(addDropDown):added drop-down in user navbars

--- src/app/componenti/navbarimpostutente/navbarimpostutente.component.ts
+++ src/app/componenti/navbarimpostutente/navbarimpostutente.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { UserManualComponent } from '../user-manual/user-manual.component';
+import { DropdownMenuComponent } from "../drop-down/drop-down.component";
 
 @Component({
-  selector: 'app-navbarimpostutente',
-  standalone: true,
-  imports: [ButtonModule,UserManualComponent],
-  templateUrl: './navbarimpostutente.component.html',
-  styleUrl: './navbarimpostutente.component.css'
+    selector: 'app-navbarimpostutente',
+    standalone: true,
+    templateUrl: './navbarimpostutente.component.html',
+    styleUrl: './navbarimpostutente.component.css',
+    imports: [ButtonModule, UserManualComponent, DropdownMenuComponent]
 })
 export class NavbarimpostutenteComponent {
 
