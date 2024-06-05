7922c2feafa60c12b412711d64926951c654031f
Gaetano Alessandretto <gaetanoalessandrettowork@gmail.com>
2024-06-05 15:32:44 +0200

    feat(service-post-register):creation of post request service for user insertion

---
+++ src/app/servizi/post-register.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClientModule, HttpHeaders,HttpClient } from '@angular/common/http';  
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostRegisterService {
+
+  constructor(private http: HttpClient) { }
+
+  private apiUrl = '.../api/v1/users/signup';  // URL dell'API
+
+  sendData(data: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    return this.http.post<any>(this.apiUrl, data, { headers });
+  }
+
+
+
+
+
+
+
+
+
+
+
+}
