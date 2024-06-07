b8dd2d625ed40fdfd34564e176d8077a87988b77
Gaetano Alessandretto <gaetanoalessandrettowork@gmail.com>
2024-06-07 10:56:24 +0200

    feat(service-login):creation of service for login

--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FloatLabelModule } from 'primeng/floatlabel';
@@ -6,6 +6,8 @@ import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
+import { ServiceloginService } from '../../servizi/servicelogin.service';
+import { User } from '../../model/userModel';
 
 @Component({
   selector: 'app-login',
@@ -24,7 +26,7 @@ import { RippleModule } from 'primeng/ripple';
 })
 export class LoginComponent {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,@Inject(ServiceloginService) private servizio: ServiceloginService) { }
 
   userForm = new FormGroup({
     username: new FormControl('', [Validators.required, Validators.email]),
@@ -32,11 +34,26 @@ export class LoginComponent {
   });
   
   onSubmit() {
-    if(this.userForm.valid) {
-      console.log(this.userForm.value);
+    if (
+      !this.userForm.value.username ||
+      !this.userForm.value.password
+    ) {
+      console.error('Error!');
+    } else if (this.userForm.valid) {
+      console.log('Form Submitted!', this.userForm.value);
+      const postData  = {
+        email: this.userForm.value.username,
+        password: this.userForm.value.password,
+      };
+      this.servizio
+        .getUser(postData)
+        .subscribe({ next: (result: any) => console.log(result) });
+      console.log(postData);
+    } else {
+      console.log('Form not valid');
     }
   }
-
+  
   navigateForgotPassword() {
     this.router.navigate(['emaildimenticata']);
   }
