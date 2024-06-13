8268430cb200640d2a765cc0797a251d0abb1ab6
c.raia <calogeroraia3@gmail.com>
2024-06-13 14:37:31 +0200

    create-dropdown-navbar-user-admin

--- src/app/componenti/navbar-admin/navbar-admin.component.ts
+++ src/app/componenti/navbar-admin/navbar-admin.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
 import { ButtonModule } from 'primeng/button'
 import { RouterLink } from '@angular/router';
+import { DropdownMenuComponent } from "../drop-down/drop-down.component";
 
 @Component({
-  selector: 'app-navbar-admin',
-  standalone: true,
-  imports: [MenubarModule, ButtonModule, RouterLink],
-  templateUrl: './navbar-admin.component.html',
-  styleUrl: './navbar-admin.component.css'
+    selector: 'app-navbar-admin',
+    standalone: true,
+    templateUrl: './navbar-admin.component.html',
+    styleUrl: './navbar-admin.component.css',
+    imports: [MenubarModule, ButtonModule, RouterLink, DropdownMenuComponent]
 })
 export class NavbarAdminComponent {
 
--- src/app/componenti/navbarutente/navbarutente.component.ts
+++ src/app/componenti/navbarutente/navbarutente.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
 import { ButtonModule } from 'primeng/button'
 import { RouterLink } from '@angular/router';
+import { DropdownMenuComponent } from "../drop-down/drop-down.component";
 
 @Component({
-  selector: 'navbarutente',
-  templateUrl: './navbarutente.component.html',
-  styleUrls: ['./navbarutente.component.css'],
-  standalone: true,
-  imports: [MenubarModule, ButtonModule, RouterLink]
+    selector: 'navbarutente',
+    templateUrl: './navbarutente.component.html',
+    styleUrls: ['./navbarutente.component.css'],
+    standalone: true,
+    imports: [MenubarModule, ButtonModule, RouterLink, DropdownMenuComponent]
 })
 export class NavbarComponent{
 
