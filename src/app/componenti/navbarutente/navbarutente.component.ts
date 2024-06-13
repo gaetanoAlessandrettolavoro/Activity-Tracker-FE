f5adcb20c6870dd4de1d0f33156c268a1aa18fb6
Gaetano Alessandretto <gaetanoalessandrettowork@gmail.com>
2024-06-13 17:40:43 +0200

    feat(addactivity-user):Adding daily activity to the user

--- src/app/componenti/navbarutente/navbarutente.component.ts
+++ src/app/componenti/navbarutente/navbarutente.component.ts
@@ -3,15 +3,23 @@ import { MenubarModule } from 'primeng/menubar';
 import { ButtonModule } from 'primeng/button'
 import { RouterLink } from '@angular/router';
 import { DropdownMenuComponent } from "../drop-down/drop-down.component";
+import { CommonModule } from '@angular/common';
+import { UserTaskCreationComponent } from '../user-task-creation/user-task-creation.component';
 
 @Component({
     selector: 'navbarutente',
     templateUrl: './navbarutente.component.html',
     styleUrls: ['./navbarutente.component.css'],
     standalone: true,
-    imports: [MenubarModule, ButtonModule, RouterLink, DropdownMenuComponent]
+    imports: [MenubarModule, ButtonModule, RouterLink, DropdownMenuComponent,CommonModule,UserTaskCreationComponent]
 })
 export class NavbarComponent{
+
+    visible = false
+
+    showDialog(){
+      this.visible = !this.visible
+    }
 
 
 }
--- src/app/componenti/user-task-creation/user-task-creation.component.ts
+++ src/app/componenti/user-task-creation/user-task-creation.component.ts
@@ -1,17 +1,9 @@
-
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { FloatLabelModule } from 'primeng/floatlabel';
-import {
-  FormControl,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
-import { MessageService } from 'primeng/api';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CheckboxModule } from 'primeng/checkbox';
 import { DialogModule } from 'primeng/dialog';
@@ -22,10 +14,17 @@ import { PasswordModule } from 'primeng/password';
 import { ToastModule } from 'primeng/toast';
 import { DropdownModule } from 'primeng/dropdown';
 import { CalendarModule } from 'primeng/calendar';
+import { AdminAddActivityForUserService } from '../../servizi/admin-add-activity-for-user.service';
+import { Activity } from '../../models/activityModel';
+import { Task } from '../../models/taskModel';
 import { TasksService } from '../../servizi/tasks.service';
+import { TaskResponse } from '../../models/taskResponseModel';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 interface City {
-  name: string;
+  taskName: string;
+  taskId: any;
 }
 
 @Component({
@@ -52,57 +51,87 @@ interface City {
   templateUrl: './user-task-creation.component.html',
   styleUrls: ['./user-task-creation.component.css']
 })
-export class UserTaskCreationComponent implements OnInit {
+export class UserTaskCreationComponent {
+  constructor(
+    private addService: AdminAddActivityForUserService,
+    private tasksServ: TasksService,
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) {}
 
-  constructor(private tasks: TasksService) {}
-  
-  cities: City[] = [];
-  selectedCity: City | undefined;
-  userForm!: FormGroup;
-
-  visible: boolean = false;
-
-  showDialog() {
-    this.visible = true;
+  firstName: any;
+  lastName: any;
+  value: any;
+  getUserId() {
+    throw new Error('Method not implemented.');
   }
 
-  ngOnInit() {
-    this.tasks.getAllTasks().subscribe({
-      next: (result: any) => {
-        result.data.document.forEach((element: any) => {
-          this.cities.push({
-            name: element.taskName
-          });
-        });
-        console.log(this.cities)
-      },
-      error: (error: any) => {
-        console.error('Si è verificato un errore:', error);
-      }
-    });
-    this.userForm = new FormGroup({
-      inputtextarea: new FormControl('', [
-        Validators.required,
-        Validators.maxLength(100),
-      ]),
-      dataattuale: new FormControl('', Validators.required),
-      orariodiinizio: new FormControl('', Validators.required),
-      orariodifine: new FormControl('', Validators.required),
-      selectedActivity: new FormControl('', Validators.required),
-    });
+  tasks: Task[] = [];
+  selectedCity: Task | undefined;
+  userForm!: FormGroup;
+
+  visible: boolean = true;
 
   
+
+  ngOnInit() {
+    this.tasksServ.getAllTasks().subscribe((result: TaskResponse) => (this.tasks = result.data.document));
+
+    this.userForm = new FormGroup({
+      notes: new FormControl('', [Validators.required, Validators.maxLength(100)]),
+      activityDate: new FormControl('', Validators.required),
+      startTime: new FormControl('', Validators.required),
+      endTime: new FormControl('', Validators.required),
+      selectedTask: new FormControl('', Validators.required),
+    });
   }
 
   onSubmit() {
     if (this.userForm.valid) {
-      console.log('Form Submitted!', this.userForm.value);
+      let date = this.userForm.get('activityDate')?.value.split('-');
+      let year = parseInt(date[0]);
+      let month = parseInt(date[1]) - 1;
+      let day = parseInt(date[2]);
+      let start = this.userForm.get('startTime')?.value.split(':');
+      let startH = parseInt(start[0]);
+      let startM = parseInt(start[1]);
+      let end = this.userForm.get('endTime')?.value.split(':');
+      let endH = parseInt(end[0]);
+      let endM = parseInt(end[1]);
+      let email = this.userForm.get('email')?.value;
+      const selectedTask = this.userForm.get('selectedTask')?.value;
+      const activityToSend: Activity = {
+        activityDate: new Date(year, month, day),
+        startTime: new Date(year, month, day, startH, startM),
+        endTime: new Date(year, month, day, endH, endM),
+        taskName: selectedTask.taskName,
+        taskID: selectedTask._id,
+        notes: this.userForm.get('notes')?.value,
+      };
+      this.getToken(activityToSend);
     } else {
       console.log('Form not valid');
     }
   }
-}
 
+  getToken(data: any) {
+    console.log(data)
+    this.route.params.subscribe((params) => {
+      this.newPassword(data).subscribe({
+        next: (result: any) => {
+          console.log('ciao');
+        },
+        error: (error: any) => {
+          console.error('Si è verificato un errore:', error);
+        },
+      });
+    });
+  }
+
+  newPassword(data: any): Observable<any> {
+    return this.http.post<any>('http://localhost:3000/api/v1/activities', data,{ withCredentials: true });
+  }
+}
 
 
 
