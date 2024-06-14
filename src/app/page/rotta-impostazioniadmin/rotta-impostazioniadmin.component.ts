227b95d9416472e1f70e1d479e87eea5d363d813
erika_cimi <erika.ciminelli@gmail.com>
2024-06-14 16:48:20 +0200

    add-link-impostazioniadmin

--- src/app/page/rotta-impostazioniadmin/rotta-impostazioniadmin.component.ts
+++ src/app/page/rotta-impostazioniadmin/rotta-impostazioniadmin.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-impostazioniadmin',
   templateUrl: './rotta-impostazioniadmin.component.html',
   styleUrls: ['./rotta-impostazioniadmin.component.css'],
-  standalone: true
+  standalone: true,
+  imports:[RouterLink]
 })
 export class AdminrouteComponent {
 CambioPassword() {
