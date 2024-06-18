040b66d3ed57f3922ba5388f377e52b67ac4c72a
Rita <rita.ciaramitaro@stevejobs.academy>
2024-06-18 16:04:38 +0200

    feat(sidebar): -added sidebar to admin vis tutte att users

--- src/app/componenti/admin-vis-tutte-att-users/admin-vis-tutte-att-users.component.ts
+++ src/app/componenti/admin-vis-tutte-att-users/admin-vis-tutte-att-users.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { DropdownMenuComponent } from '../drop-down/drop-down.component';
 import { UserManualComponent } from '../user-manual/user-manual.component';
+import { SidebarAdminComponent } from "../sidebar-admin/sidebar-admin.component";
 
 @Component({
-  selector: 'app-admin-vis-tutte-att-users',
-  standalone: true,
-  imports: [ButtonModule, RouterLink,DropdownMenuComponent,UserManualComponent],
-  templateUrl: './admin-vis-tutte-att-users.component.html',
-  styleUrl: './admin-vis-tutte-att-users.component.css'
+    selector: 'app-admin-vis-tutte-att-users',
+    standalone: true,
+    templateUrl: './admin-vis-tutte-att-users.component.html',
+    styleUrl: './admin-vis-tutte-att-users.component.css',
+    imports: [ButtonModule, RouterLink, DropdownMenuComponent, UserManualComponent, SidebarAdminComponent]
 })
 export class AdminVisTutteAttUsersComponent {
+  @ViewChild(SidebarAdminComponent) sidebarAdminComponent!: SidebarAdminComponent;
+
 
 }
