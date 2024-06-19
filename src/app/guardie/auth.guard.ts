b07b71f03758c1dc51dc026394edbb506af10052
Gaetano Alessandretto <gaetanoalessandrettowork@gmail.com>
2024-06-19 10:45:36 +0200

    fix(guard):if the admin is not logged in he goes to the home page

--- src/app/guardie/auth.guard.ts
+++ src/app/guardie/auth.guard.ts
@@ -5,31 +5,14 @@ import { ServiceloginService } from '../servizi/servicelogin.service';
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(ServiceloginService);
   const router = inject(Router);
-  
-  // controlla se l'utente è autenticato
-  if (!authService.isUtente()) {
-    router.navigate(['home']);
-    return false;
-  }
-  
-  //rotte amministratore
-  if (state.url.startsWith('/homeadmin') || state.url.startsWith('/impostazioniadmin') || state.url.startsWith('/admin-vis-utente-specifico/:id')) {
-    if (!authService.isAdmin()) {
-      router.navigate(['/userhome']);
-      return false;
-    }
-    return true;
-  }
-  
- //rotte utente
-  if (state.url.startsWith('/userhome') || state.url.startsWith('/impostazioniutente') || state.url.startsWith('/attivitarecentiutente')) {
-    if (authService.isAdmin()) {
-      router.navigate(['/homeadmin']);
-      return false;
-    }
-    return true;
-  }
 
   
-  return true;
+   
+  if (localStorage.getItem("admin")) {
+    return true;
+  } else {
+    router.navigate(['/home']);
+    return false;
+  }
 };
+
