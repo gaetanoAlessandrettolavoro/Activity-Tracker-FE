8aed45a3105eaf7307580edfde0a6b6b8972f886
gian <gianmarco.clu23@gmail.com>
2024-06-22 17:56:53 +0200

    feat(interceptors): first implementation of interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,9 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { errorsInterceptor } from './interceptors/errors.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient()],
+  providers: [provideRouter(routes), provideAnimations(), provideHttpClient(withInterceptors([errorsInterceptor]))],
 };
