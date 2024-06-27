414a39f2340576007500c5a34a63e8dce086d642
Gaetano Alessandretto <gaetanoalessandrettowork@gmail.com>
2024-06-27 10:14:49 +0200

    fix(paginator-home-user): fix totalDocuments

--- src/app/componenti/daily-activity/daily-activity.component.ts
+++ src/app/componenti/daily-activity/daily-activity.component.ts
@@ -1,4 +1,3 @@
-
 import { CommonModule, DatePipe } from '@angular/common';
 import { TableModule } from 'primeng/table';
 import {
@@ -10,7 +9,6 @@ import {
   signal,
 } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
-
 import { EditActivityButtonComponent } from '../edit-activity-button/edit-activity-button.component';
 import { Activity } from '../../models/activityModel';
 import { DeleteActivityButtonComponent } from '../delete-activity-button/delete-activity-button.component';
@@ -56,67 +54,67 @@ export class DailyActivityComponent {
   rows: number = 10;
   totale! : number
   conteggio! : string
-  constructor(private adminService: AdminserviceService, private messageService: MessageService, private userService: UserServiceService, private router: Router, private errors: ErrorServiziService) {
+
+  constructor(
+    private adminService: AdminserviceService, 
+    private messageService: MessageService, 
+    private userService: UserServiceService, 
+    private router: Router, 
+    private errors: ErrorServiziService
+  ) {
     this.oggettistampati = [];
-
+    this.fetchActivities();
+    
     effect(() => {
-      console.log(this.limit + "e" + this.page)
-      this.adminService.getActivitiesByDate({pageNumber : this.page,limit : this.limit}).subscribe({
-        next: (result: any) => {
-          console.log(result)
-          this.conteggio = this.limit + " di " + result.results
-          if (result.data.userActivities.length === 0) {
-            console.log('non ci sono dati');
-            this.nodailyactivity = true;
-            this.dailyactivitycomponent = false;
-          } else {
-            this.nodailyactivity = false;
-            this.dailyactivitycomponent = true;
-            this.oggettistampati = [];
-            result.data.userActivities.forEach((element: any) => {
-              let obj = {
-                _id: element._id,
-                taskName: element.taskName,
-                taskID: element.taskID,
-                startTime: new Date(element.startTime),
-                endTime: new Date(element.endTime),
-                notes: element.notes,
-                userID: element.userID,
-                isActive: element.isActive,
-              };
-              this.oggettistampati.push(obj);
-            });
+      if (this.dailyActivity()) {
+        this.fetchActivities();
+      }
+    });
+  }
 
-            console.log(this.oggettistampati);
-          }
-        },
-        error: (err) => {
-          let numberValue = parseInt(
-            localStorage.getItem('submit') ?? '0',
-            10
-          );
-
-          if (numberValue < 4) {
-            numberValue++; // Incrementa il contatore di tentativi
-            localStorage.setItem('submit', numberValue.toString());
-          }
-
-          if(numberValue == 4){
-            setTimeout(() => {
-              localStorage.removeItem('submit');
-            }, 5000);
-          }
-
-          if (numberValue < 4) {
-            this.showError(err.status);
-          }
+  fetchActivities() {
+    this.adminService.getActivitiesByDate({ pageNumber: this.page, limit: this.limit }).subscribe({
+      next: (result: any) => {
+        console.log(result)
+        this.conteggio = result.results + " di " + result.totalDocuments;
+        if (result.data.userActivities.length === 0) {
+          this.nodailyactivity = true;
+          this.dailyactivitycomponent = false;
+        } else {
+          this.nodailyactivity = false;
+          this.dailyactivitycomponent = true;
+          this.oggettistampati = result.data.userActivities.map((element: any) => ({
+            _id: element._id,
+            taskName: element.taskName,
+            taskID: element.taskID,
+            startTime: new Date(element.startTime),
+            endTime: new Date(element.endTime),
+            notes: element.notes,
+            userID: element.userID,
+            isActive: element.isActive,
+          }));
         }
-      });
+      },
+      error: (err) => {
+        let numberValue = parseInt(localStorage.getItem('submit') ?? '0', 10);
+        if (numberValue < 4) {
+          numberValue++; // Incrementa il contatore di tentativi
+          localStorage.setItem('submit', numberValue.toString());
+        }
+        if (numberValue == 4) {
+          setTimeout(() => {
+            localStorage.removeItem('submit');
+          }, 5000);
+        }
+        if (numberValue < 4) {
+          this.showError(err.status);
+        }
+      }
     });
   }
 
   showError(statusCode: number) {
-    if(statusCode === 401 || statusCode === 429) {
+    if (statusCode === 401 || statusCode === 429) {
       this.messageService.add(this.errors.getErrorMessage(statusCode));
       setTimeout(() => {
         this.userService.logout();
@@ -132,176 +130,13 @@ export class DailyActivityComponent {
   }
 
   changeLimit() {
-    this.adminService.getActivitiesByDate().subscribe({
-         next: (result : any) => {
-            if(this.limit > result.results){
-                console.log("no")
-                this.limit = 4
-                this.adminService.getActivitiesByDate({pageNumber : this.page,limit : this.limit}).subscribe({
-                  next: (result: any) => {
-                    console.log(result.results)
-                    this.conteggio = this.limit + " di " + result.results
-                    if (result.data.userActivities.length === 0) {
-                      console.log('non ci sono dati');
-                      this.nodailyactivity = true;
-                      this.dailyactivitycomponent = false;
-                    } else {
-                      console.log('ci sono dati');
-                      this.nodailyactivity = false;
-                      this.dailyactivitycomponent = true;
-                      this.oggettistampati = [];
-                      result.data.userActivities.forEach((element: any) => {
-                        let obj = {
-                          _id: element._id,
-                          taskName: element.taskName,
-                          taskID: element.taskID,
-                          startTime: new Date(element.startTime),
-                          endTime: new Date(element.endTime),
-                          notes: element.notes,
-                          userID: element.userID,
-                          isActive: element.isActive,
-                        };
-                        this.oggettistampati.push(obj);
-                      });
-            
-                      console.log(this.oggettistampati);
-                    }
-                  },
-                  error: (err) => {
-                    let numberValue = parseInt(
-                      localStorage.getItem('submit') ?? '0',
-                      10
-                    );
-            
-                    if (numberValue < 4) {
-                      numberValue++; // Incrementa il contatore di tentativi
-                      localStorage.setItem('submit', numberValue.toString());
-                    }
-            
-                    if(numberValue == 4){
-                      setTimeout(() => {
-                        localStorage.removeItem('submit');
-                      }, 5000);
-                    }
-            
-                    if (numberValue < 4) {
-                      this.showError(err.status);
-                    }
-                  }
-                });
-            }
-            else{
-              this.adminService.getActivitiesByDate({pageNumber : this.page,limit : this.limit}).subscribe({
-                next: (result: any) => {
-                  if (result.data.userActivities.length === 0) {
-                    console.log('non ci sono dati');
-                    this.nodailyactivity = true;
-                    this.dailyactivitycomponent = false;
-                  } else {
-                    console.log(result)
-                    this.conteggio = this.limit + " di " + result.results
-                    console.log('ci sono dati');
-                    this.nodailyactivity = false;
-                    this.dailyactivitycomponent = true;
-                    this.oggettistampati = [];
-                    result.data.userActivities.forEach((element: any) => {
-                      let obj = {
-                        _id: element._id,
-                        taskName: element.taskName,
-                        taskID: element.taskID,
-                        startTime: new Date(element.startTime),
-                        endTime: new Date(element.endTime),
-                        notes: element.notes,
-                        userID: element.userID,
-                        isActive: element.isActive,
-                      };
-                      this.oggettistampati.push(obj);
-                    });
-          
-                    console.log(this.oggettistampati);
-                  }
-                },
-                error: (err) => {
-                  let numberValue = parseInt(
-                    localStorage.getItem('submit') ?? '0',
-                    10
-                  );
-          
-                  if (numberValue < 4) {
-                    numberValue++; // Incrementa il contatore di tentativi
-                    localStorage.setItem('submit', numberValue.toString());
-                  }
-          
-                  if(numberValue == 4){
-                    setTimeout(() => {
-                      localStorage.removeItem('submit');
-                    }, 5000);
-                  }
-          
-                  if (numberValue < 4) {
-                    this.showError(err.status);
-                  }
-                }
-              });
-            }
-         }
-    })
-    
+    this.fetchActivities();
   }
 
   onPageChange(event: any) {
     const pageNumber = event.page + 1;
-    this.page = pageNumber
-    this.adminService.getActivitiesByDate({pageNumber : pageNumber,limit : this.limit}).subscribe({
-      next: (result: any) => {
-        this.conteggio = this.limit + "di" + result.results
-        if (result.data.userActivities.length === 0) {
-          console.log('non ci sono dati');
-          this.nodailyactivity = true;
-          this.dailyactivitycomponent = false;
-        } else {
-          console.log('ci sono dati');
-          this.nodailyactivity = false;
-          this.dailyactivitycomponent = true;
-          this.oggettistampati = [];
-          result.data.userActivities.forEach((element: any) => {
-            let obj = {
-              _id: element._id,
-              taskName: element.taskName,
-              taskID: element.taskID,
-              startTime: new Date(element.startTime),
-              endTime: new Date(element.endTime),
-              notes: element.notes,
-              userID: element.userID,
-              isActive: element.isActive,
-            };
-            this.oggettistampati.push(obj);
-          });
-
-          console.log(this.oggettistampati);
-        }
-      },
-      error: (err) => {
-        let numberValue = parseInt(
-          localStorage.getItem('submit') ?? '0',
-          10
-        );
-
-        if (numberValue < 4) {
-          numberValue++; // Incrementa il contatore di tentativi
-          localStorage.setItem('submit', numberValue.toString());
-        }
-
-        if(numberValue == 4){
-          setTimeout(() => {
-            localStorage.removeItem('submit');
-          }, 5000);
-        }
-
-        if (numberValue < 4) {
-          this.showError(err.status);
-        }
-      }
-    });
+    this.page = pageNumber;
+    this.fetchActivities();
   }
-  }
+
+}
