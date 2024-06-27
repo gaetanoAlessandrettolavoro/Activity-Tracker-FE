0fa8a9a48871bbc182461be68948332912712717
gian <gianmarco.clu23@gmail.com>
2024-06-27 13:17:58 +0200

    feat(charts-service): updated service)

--- src/app/servizi/charts.service.ts
+++ src/app/servizi/charts.service.ts
@@ -13,6 +13,9 @@ export interface BasicData {
       backgroundColor: string[];
       borderColor: string[];
       borderWidth: number;
+      userID: string[];
+      _id: string[];
+      taskName: string[];
     },
   ];
 }
@@ -23,16 +26,18 @@ export interface BasicData {
 export class ChartsService {
   constructor(private http: HttpClient) {}
 
-  hoursPerActivity(): Observable<BasicData[]> {
+  hoursPerActivity(date: Date): Observable<BasicData[]> {
+    const [year, month, day] = [date.getFullYear(), date.getMonth(), date.getDate()+1];
+    const thisDay = new Date(year, month, day).toISOString().split('T')[0];
+    const nextDay = new Date(year, month, day+1).toISOString().split('T')[0];
     const apiUrl =
       'http://localhost:3000/api/v1/activities/hoursPerActivity?isActive=true';
-      
-    return this.http.get(apiUrl, { withCredentials: true }).pipe(
+    return this.http.get(`${apiUrl}&startTime[gte]=${thisDay}&startTime[lt]=${nextDay}`, { withCredentials: true }).pipe(
       map((res: any) => {
         if (res && res.data && res.data.activities) {
           let basicData: BasicData = {
             labels: res.data.activities.map((act: any) => {
-              return `${act.taskName} - ${act.userID}`}),
+              return `${act.taskName}`}),
             datasets: [
               {
                 label: 'Ore per attività',
@@ -40,6 +45,9 @@ export class ChartsService {
                 backgroundColor: ['rgba(255, 159, 64, 0.2)'],
                 borderColor: ['rgb(255, 159, 64)'],
                 borderWidth: 1,
+                userID: res.data.activities.map((act: any) => act.userID),
+                _id: res.data.activities.map((act: any) => act._id),
+                taskName: res.data.activities.map((act: any) => act.taskName)
               },
             ],
           };
