6529e014e28373fbcd4dfb23be8b5697a33bcd7c
gian <gianmarco.clu23@gmail.com>
2024-06-30 19:19:20 +0200

    fix(tasks-page): modified filter variable

--- src/app/page/task-crud/task-crud.component.ts
+++ src/app/page/task-crud/task-crud.component.ts
@@ -42,7 +42,7 @@ export class TaskComponent implements OnInit {
   states: string[] = ['To do', 'In progress', 'Done'];
   progress: number = 0;
   filterOptions: string[] = ['Solo attive', 'Solo non attive', 'Tutte'];
-  filter: string = 'Tutte';
+  filter: string = this.filterOptions[2];
 
   editTaskForm = new FormGroup({
     taskName: new FormControl(this.taskToEdit().taskName, Validators.required),
