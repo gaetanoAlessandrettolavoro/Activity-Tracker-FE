6755d5efee31184b120087543fc1dbe1ab99be45
gian <gianmarco.clu23@gmail.com>
2024-08-01 16:30:06 +0200

    fix(captcha): fixed service

--- src/app/servizi/captcha.service.ts
+++ src/app/servizi/captcha.service.ts
@@ -1,23 +1,19 @@
 import { Injectable } from '@angular/core';
-import axios from 'axios';
-import { environment } from '../../environments/environment';
+import { alias } from './defines';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CaptchaService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  async verifyCaptcha(token: string) {
-    const secretKey = environment.recaptcha.secretKey;
-    const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`;
+  verifyCaptcha(token: string): Observable<any> {
+    const url = `http://${alias}:3000/api/v1/captcha`;
 
-    try {
-      const response = await axios.post(url);
-      return response.data.success;
-    } catch (error) {
-      console.error('Errore nella verifica del CAPTCHA:', error);
-      return false;
-    }
+    const body = {'g-recaptcha-response': token}
+
+    return this.http.post(url, body);
   }
 }
